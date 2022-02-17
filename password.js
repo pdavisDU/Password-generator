83e180dfd5692a37486be2ac6a4f1637340f193e
pdavisDU <pdavisdu@gmail.com>
2022-02-17 13:00:07 -0700

    second commit got it working

--- password.js
+++ password.js
@@ -1,31 +1,68 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//var passwordDisplay = document.querySelector("#password")
 
-generateBtn.addEventListener("click", writePassword);
 
-var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "`", "-", "_", "=", "+", ",", ".", "<", ">"];
+var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lettersLower = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var specialChar = "!@#$%^&*()-_=+";
 
-var length = "";
-
-document.querySelector("#generate").addEventListener("click", function() {
-  var userClick = prompt("Please choose a password between 8-128 characters")
-
-})
-
+var aLength = "";
 
 // Write password to the #password input
 //recursion - function calls itself call generate password from inside itself to restart it
 // data structure for special char and letters -- arrays for them
 function writePassword() {
-  var password = generatePassword();
+  var passwordpassword = generatePassword();
   var passwordText = document.querySelector("#password");
   
-  passwordText.value = password;
+  passwordText.value = passwordpassword;
   
 }  
 // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 //need to create a generate password that will return 
+
+function generatePassword() {
+  var password = "";
+  
+  var pLength = prompt("Choose password length");
+
+  if (pLength < 8 && pLength > 128) {
+    alert("Ivalid: Must be between 8 and 128 characters");
+    generatePassword();
+  }
+
+  var lUpper = prompt("Do you want upper case characters? (Yes or No")
+
+  if (lUpper === "Yes") {
+    aLength += lettersUpper
+  }
+
+  var lLower = prompt("Do you want lower case characters? (Yes or No)")
+
+  if (lLower === "Yes") {
+    aLength += lettersLower
+  }
+
+  var spec = prompt("Do you want special characters? (Yes or No)")
+
+  if (spec === "Yes") {
+    aLength += specialChar
+  }
+
+  var numbers = prompt("Do you want numbers? (Yes or No)")
+
+  if (numbers === "Yes") {
+    aLength += number
+  }
+
+for (var i=0; i < pLength; i++){
+  password += aLength.charAt( Math.floor(Math.random() * aLength.length));
+}
+
+return password;
+}
+
